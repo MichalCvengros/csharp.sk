cd6f5e8b256ca500c86fb1d1e3dda5662c166476
mcvengros66@gmail.com <mcvengros66@gmail.com>
2025-10-15 13:00:05 +0200

    ...

---
+++ Name and age/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Name?");
+string? name = Console.ReadLine();
+
+Console.WriteLine("Age?");
+string? age = Console.ReadLine();
+
+Console.WriteLine($"Ahoj {name}. Tvoj vek je {age}");
---
+++ ZATIALLLLL NEVIEM/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string meno = ZiskajMeno();
+string vek = ZiskajVek();
+
+Console.WriteLine($"Ahoj {meno}. Tvoj vek je {vek}");
+
+string ZiskajMeno()
+{
+    Console.WriteLine("Zadaj Meno");
+    string meno = Console.ReadLine();
+    return meno;
+}
+string ZiskajVek()
+{
+    Console.WriteLine("Zadaj vek");
+    string vek = Console.ReadLine();
+    return vek;
+}
+
+void VypisUdaje(string meno, string vek)
+{
+    Console.WriteLine($"Ahoj {meno}. Tvoj vek je {vek} ");
+}
